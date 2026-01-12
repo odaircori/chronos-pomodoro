dad6823736f7a8709ee03fee155d248962207a5b
Odair <odaircori@gmail.com>
2026-01-12 10:37:03 -0300

    Organizando

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from './App'
+import { Heading } from './components/Heading'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Heading attr='StringNormal' attr2={1234}>Olá Mundo do React 1</Heading>
+    <App/>
+    <h1>Olá mundo</h1>
   </StrictMode>,
 )
