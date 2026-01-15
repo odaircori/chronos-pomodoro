b6f1874bf1dae5982ef8fc71cf50604d78a581e6
Odair <odaircori@gmail.com>
2026-01-14 21:14:56 -0300

    Implementando mudança de tema com useStaste useEffect

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,69 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
-import styles from './styles.module.css'
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
 
-export function Menu(){
-    return (
+export function Menu() {
+  type AvailableTheme = "dark" | "light";
+
+  const [theme, setTheme] = useState<AvailableTheme>("dark");
+
+  function handleThemeChange(event: React.MouseEvent) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    console.log("USeEffect rodando");
+
+    document.documentElement.setAttribute("data-theme", theme);
+
+    //este return é executado pelo useEffect antes mesmo de executar a ação dele. Serve para executar limpezas na página, como remover listerners, antes do componente ser atualizado
+    return () => {
+        console.log('Este componente será atualizado');
+    }
+  }, [theme]);
+
+  return (
     <div className={styles.menu}>
-        <a className={styles.menuLink} href="#">
-            <HouseIcon />
-        </a>
-        <a className={styles.menuLink} href="#">
-            <HistoryIcon />
-        </a>
-        <a className={styles.menuLink} href="#">
-            <SettingsIcon />
-        </a>                
-        <a className={styles.menuLink} href="#">
-            <SunIcon />
-        </a>        
+      <h1>{theme}</h1>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para Home"
+        title="Ir para Home"
+      >
+        <HouseIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Histórico"
+        title="Histórico"
+      >
+        <HistoryIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configurações"
+        title="Configurações"
+      >
+        <SettingsIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Mudar Tema"
+        title="Mudar Tema"
+        onClick={handleThemeChange}
+      >
+        <SunIcon />
+      </a>
     </div>
-    )
+  );
 }
