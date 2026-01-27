8fcb5256918119db9d6b8f48b9bba74afd2c2bfa
Odair <odaircori@gmail.com>
2026-01-27 20:09:21 -0300

    Capturando submit do form

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,12 +2,22 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useState } from "react";
 
 export function MainForm(){
+    const [taskName, setTaskName] = useState('')
+
+    function handleCreateNewTask(evt: React.FormEvent){
+        evt.preventDefault();
+
+        console.log("TESTE")
+
+    }
+
     return (
-    <form>
+    <form onSubmit={handleCreateNewTask}>
         <div className="formRow">
-            <DefaultInput id='input' type='text' labelText='task:' placeholder='Digite algo'/>
+            <DefaultInput id='input' type='text' labelText='task:' placeholder='Digite algo' value={taskName} onChange={e => setTaskName(e.target.value)}/>
         </div>
         <div className="formRow">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, officiis!</p>
