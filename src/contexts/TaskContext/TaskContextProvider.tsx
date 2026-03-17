ee7154b089529bd1f3b536a84b4c6e104dbaa83e
Odair <odaircori@gmail.com>
2026-03-17 18:25:09 -0300

    Tocando áudio ao finalizar a contagem

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import {useEffect, useReducer} from "react";
+import {useEffect, useReducer, useRef} from "react";
 import {initialTaskState} from "./initialTaskState";
 import {TaskContext} from "./TaskContext";
 import {taskReducer} from "./taskReducer.ts";
 import {TimerWorkerManager} from "../../workers/TimerWorkerManager.ts";
 import {TaskActionTypes} from "./taskActionTypes.ts";
+import {loadBeep} from "../../utils/loadBeep.ts";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -13,12 +14,18 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
   const worker = TimerWorkerManager.getInstance();
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   worker.onMessage((event: MessageEvent) => {
     const countDownSeconds = event.data;
-    console.log(countDownSeconds);
 
     if(countDownSeconds <= 0) {
+      if(playBeepRef.current) {
+        console.log("Tocando áudio")
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
+
       console.log("Worker finalizado porque a task completou");
       dispatch({type: TaskActionTypes.COMPLETE_TASK});
       worker.terminate();
@@ -35,6 +42,19 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
       worker.postMessage(state);
   },[worker, state]);
+
+  useEffect(() => {
+    console.log("Active task mudou", state.activeTask);
+
+    if(state.activeTask && playBeepRef.current === null){
+      console.log("Carregando áudio");
+      playBeepRef.current = loadBeep();
+    }else{
+      console.log("Zerando áudio");
+      playBeepRef.current = null;
+    }
+
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
